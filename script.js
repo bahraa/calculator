697e8ced504b8f7d8a5d7f1e75ef8e0543765729
bahraa <bbahra98@gmail.com>
2018-09-13 16:51:44 +0300

    not finished

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const screen = $("#screen ");
+
+$(".number").on("click", function () {
+
+    const num1 = screen.html();
+    const num2 = this.innerHTML
+    const result = screen.html(Number(num1 + num2))
+
+
+
+
+})
+
+function arithmaticOp(val1, val2, operator) {
+
+    switch 
+    
+    case
+}
+
+
+    $("operator").on("click")
